c0d65812eaea109f7fed7cdc534c32accc644f7d
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-08 17:17:50 +0000

    Thai language pack

---
+++ languages/th.php
@@ -0,0 +1,23 @@
+<?php
+
+	$thai = array(
+	
+		'friendfeed' => 'FriendFeed',
+	
+		'friendfeed:username' => 'ชื่อสมาชิก FriendFeed',
+		'friendfeed:apikey' => 'Friendfeed API key',
+		'friendfeed:apikeyfind' => 'ค้นหา key ของคุณ',
+		'friendfeed:num_display' => 'จำนวนที่ต้องการแสดง',
+	
+	
+		'friendfeed:favorited' => "ความนิยม: ",
+	
+		'friendfeed:nouser' => 'วิดเจ็ตไม่สามารถตั้งค่าได้.',
+		'friendfeed:nonefound' => 'ไม่มีรายละเอียดที่จะแสดง.',
+		'friendfeed:epicfail' => 'ไม่สามารถติดต่อด้วย user\'s FriendFeed ได้ในขณะนี้',
+			
+	);
+					
+	add_translation("th",$thai);
+
+?>
