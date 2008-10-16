256f8542e42c00c2256b0083aefe3b147437cc45
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-16 09:08:10 +0000

    Friendfeed language pack

---
+++ languages/ru.php
@@ -0,0 +1,23 @@
+<?php
+
+	$russian = array(
+	
+		'friendfeed' => 'FriendFeed',
+	
+		'friendfeed:username' => 'Имя пользователя для FriendFeed',
+		'friendfeed:apikey' => 'Ключ API FriendFeed',
+		'friendfeed:apikeyfind' => 'найти ваш ключ',
+		'friendfeed:num_display' => 'Количество показываемых записей',
+	
+	
+		'friendfeed:favorited' => "Добавили в избранное: ",
+	
+		'friendfeed:nouser' => 'Этот виджет ещё не настроен.',
+		'friendfeed:nonefound' => 'Нет информации.',
+		'friendfeed:epicfail' => 'Не удалось соединиться с FriendFeed данного пользователя.',
+			
+	);
+					
+	add_translation("ru",$russian);
+
+?>
