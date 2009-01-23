3d0a8e7938f099396e144dfc993055c6a7d6defc
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-01-23 15:48:20 +0000

    Refs #706

--- start.php
+++ start.php
@@ -6,7 +6,7 @@
 	 * @package ElggFriendfeed
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
 	 * @author Ben Werdmuller <ben@elgg.com>
-	 * @copyright Curverider Ltd 2008
+	 * @copyright Curverider Ltd 2008-2009
 	 * @link http://elgg.org/
 	 */
 
